d0e3aa062399a02e4847bdd46e2f4a7ce2634c61
Benoit Thore <thorebenoit@gmail.com>
2019-01-25 13:44:08 +0000

    Adding Layout creation on Context

--- EnamelAndroid/src/main/java/com/thorebenoit/enamel/android/dsl/views/layoutCreation.kt
+++ EnamelAndroid/src/main/java/com/thorebenoit/enamel/android/dsl/views/layoutCreation.kt
@@ -1,5 +1,6 @@
 package com.thorebenoit.enamel.android.dsl.views
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
@@ -23,5 +24,18 @@ inline fun ViewGroup.verticalLayout(crossinline init: LinearLayout.() -> Unit = 
 /////////
 inline fun <T : View> T.addAndInit(layout: ViewGroup, init: T.() -> Unit = {}) = apply {
     layout.addView(this)
+    init()
+}
+////////
+
+
+inline fun Context.constraintLayout(init: ConstraintLayout.() -> Unit = {}) = ConstraintLayout(this).apply(init)
+
+inline fun Context.frameLayout(crossinline init: FrameLayout.() -> Unit = {}) = FrameLayout(this).apply(init)
+
+inline fun Context.linearLayout(crossinline init: LinearLayout.() -> Unit = {}) = LinearLayout(this).apply(init)
+
+inline fun Context.verticalLayout(crossinline init: LinearLayout.() -> Unit = {}) = LinearLayout(this).apply {
+    orientation = LinearLayout.VERTICAL
     init()
 }
