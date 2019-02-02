6263cc57e3fa8aaff95a0dee7be1e20eb469c4c7
Benoit Thore <thorebenoit@gmail.com>
2019-02-02 14:40:05 +0000

    adding heap dump functions

--- EnamelKotlin/src/main/java/com/thorebenoit/enamel/kotlin/core/dumpingGround.kt
+++ EnamelKotlin/src/main/java/com/thorebenoit/enamel/kotlin/core/dumpingGround.kt
@@ -1,8 +1,28 @@
 package com.thorebenoit.enamel.kotlin.core
 
+import java.io.File
 import java.lang.Exception
+import java.lang.management.ManagementFactory
+import java.nio.charset.Charset
 
-val Any?.print get() = println(this)
+fun executeHeapDump(destinationFolder: String = "/tmp/", fileName: String = "heapdump.hprof"): String {
+    val pid = ManagementFactory.getRuntimeMXBean().name.split("@")[0]
+
+    val destinationFile = File(destinationFolder,fileName).absolutePath
+    val command = "jmap -dump:format=b,file=$destinationFile $pid"
+
+    return command.split(" ").executeInTerminal()
+}
+
+fun String.executeInTerminal() =
+    Runtime.getRuntime().exec(this)
+        .inputStream.readBytes().toString(Charset.defaultCharset())
+
+fun List<String>.executeInTerminal() =
+    Runtime.getRuntime().exec(this.toTypedArray())
+        .inputStream.readBytes().toString(Charset.defaultCharset())
+
+val <T> T.print get() = apply { println(this) }
 
 fun tryCatch(tryBlock: () -> Unit, catchBlock: () -> Unit = {}) {
     try {
