bda812d2034e05d48fc9fd34d74d3dc6b4828eb1
Benoit Thore <thorebenoit@gmail.com>
2019-03-03 14:44:20 +0000

    Update blockinGet

--- EnamelKotlin/src/main/java/com/thorebenoit/enamel/kotlin/threading/coroutineHelpers.kt
+++ EnamelKotlin/src/main/java/com/thorebenoit/enamel/kotlin/threading/coroutineHelpers.kt
@@ -3,17 +3,7 @@ package com.thorebenoit.enamel.kotlin.threading
 import com.thorebenoit.enamel.kotlin.core.tryCatch
 import kotlinx.coroutines.*
 
-fun <T> Deferred<T>.blockingGet(): T {
-    val lock = ELock()
-    var ret: T? = null
-    coroutine {
-        ret = await()
-        lock.unlock()
-    }
-
-    lock.lock()
-    return ret!!
-}
+fun <T> Deferred<T>.blockingGet(): T = runBlocking { await() }
 
 fun coroutine(block: suspend CoroutineScope.() -> Unit) = GlobalScope.launch(block = block)
 
