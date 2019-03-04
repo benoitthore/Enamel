c7131720d65991be4a961703d5f202647cae0091
Benoit Thore <thorebenoit@gmail.com>
2019-03-04 12:56:48 +0000

    Update library

--- EnamelAndroid/src/main/java/com/thorebenoit/enamel/android/dumpingGround.kt
+++ EnamelAndroid/src/main/java/com/thorebenoit/enamel/android/dumpingGround.kt
@@ -1,5 +1,8 @@
 package com.thorebenoit.enamel.android
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.ViewGroup
@@ -8,6 +11,14 @@ import java.lang.Exception
 
 const val matchParent = ViewGroup.LayoutParams.MATCH_PARENT
 const val wrapContent = ViewGroup.LayoutParams.WRAP_CONTENT
+
+
+
+fun Context.openUrl(url :String){
+    val i = Intent(Intent.ACTION_VIEW)
+    i.data = Uri.parse(url)
+    startActivity(i)
+}
 
 
 fun EditText.onTextChanged(function: (String) -> Unit) {
--- EnamelAndroid/src/main/java/com/thorebenoit/enamel/android/threading/coroutineHelpers.kt
+++ EnamelAndroid/src/main/java/com/thorebenoit/enamel/android/threading/coroutineHelpers.kt
@@ -1,8 +1,27 @@
 package com.thorebenoit.enamel.android.threading
 
+import com.thorebenoit.enamel.kotlin.threading.coroutine
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 fun mainThreadCoroutine(block: suspend CoroutineScope.() -> Unit) = GlobalScope.launch(context = Dispatchers.Main,block = block)
+
+
+
+class AndroidCoroutineRunner : CoroutineRunner {
+    override fun onMainThread(block: suspend CoroutineScope.() -> Unit) {
+        mainThreadCoroutine(block)
+    }
+
+    override fun onIO(block: suspend CoroutineScope.() -> Unit) {
+        coroutine(block)
+    }
+
+}
+
+interface CoroutineRunner {
+    fun onMainThread(block: suspend CoroutineScope.() -> Unit)
+    fun onIO(block: suspend CoroutineScope.() -> Unit)
+}
