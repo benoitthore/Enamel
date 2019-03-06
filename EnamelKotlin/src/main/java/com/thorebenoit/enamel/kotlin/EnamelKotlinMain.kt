c9a2854b03a38c8b3f89e8831094a35c6b555746
Benoit Thore <thorebenoit@gmail.com>
2019-03-06 21:14:50 +0000

    Adding Ktor file server

--- EnamelKotlin/src/main/java/com/thorebenoit/enamel/kotlin/EnamelKotlinMain.kt
+++ EnamelKotlin/src/main/java/com/thorebenoit/enamel/kotlin/EnamelKotlinMain.kt
@@ -1,26 +1,15 @@
 package com.thorebenoit.enamel.kotlin
 
 import com.thorebenoit.enamel.kotlin.core.print
+import com.thorebenoit.enamel.kotlin.server.startFileServer
 import com.thorebenoit.enamel.kotlin.threading.CoroutineLock
 import com.thorebenoit.enamel.kotlin.threading.coroutine
+import java.io.File
 
 object EnamelKotlinMain {
     @JvmStatic
     fun main(args: Array<String>) {
-        val lock = CoroutineLock{
-            "on unlock".print
-        }
-        coroutine {
-           lock.wait()
-            "unlocked inside coroutine".print
-        }
-
-        Thread.sleep(500)
-
-        lock.unlock()
-        println("DONE")
-
-        Thread.sleep(50)
+       File("/Users/benoit/Documents/").startFileServer()
     }
 
 }
