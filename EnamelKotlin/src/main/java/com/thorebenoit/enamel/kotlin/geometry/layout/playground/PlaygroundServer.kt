340726d7c0fb064b8d9e7f560030efbbcc056349
Benoit Thore <thorebenoit@gmail.com>
2019-03-26 14:27:09 +0000

    Playground update, layout can be sent but serialization still has an issue

--- EnamelKotlin/src/main/java/com/thorebenoit/enamel/kotlin/geometry/layout/playground/PlaygroundServer.kt
+++ EnamelKotlin/src/main/java/com/thorebenoit/enamel/kotlin/geometry/layout/playground/PlaygroundServer.kt
@@ -1,6 +1,12 @@
 package com.thorebenoit.enamel.kotlin.geometry.layout.playground
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.thorebenoit.enamel.kotlin.core.data.ignoreUnknownObjectMapper
 import com.thorebenoit.enamel.kotlin.geometry.layout.ELayout
+import com.thorebenoit.enamel.kotlin.geometry.layout.serializer.ELayoutDeserializer
+import com.thorebenoit.enamel.kotlin.geometry.layout.serializer.ELayoutSerializer
+import com.thorebenoit.enamel.kotlin.geometry.layout.serializer.digital.ELayoutDeserializerDigital
+import com.thorebenoit.enamel.kotlin.geometry.layout.serializer.digital.ELayoutSerializerDigital
 import io.ktor.application.call
 import io.ktor.request.receive
 import io.ktor.routing.post
@@ -14,18 +20,20 @@ class PlaygroundServer {
     }
 
     fun start(port: Int = defaultPort, onNewLayout: (ELayout) -> Unit) {
+        val mapper = ignoreUnknownObjectMapper()
         embeddedServer(Netty, port = port) {
             routing {
                 post("/") {
-                        TODO("This code is now invalid")
-//                    try {
-//                        val str = call.receive<String>()
-//                        val layout = ELayoutSerializer.deserialize(str)
-//                        println("SOCKET-IN: $str")
-//                        onNewLayout(layout)
-//                    } catch (e: Throwable) {
-//                        System.err.println(e)
-//                    }
+                    try {
+                        val json = call.receive<String>()
+                        val data = mapper.readValue<List<Number>>(json)
+
+                        println("SOCKET-IN: $data")
+                        val layout = ELayoutDeserializerDigital.createIntIDDesrializer(data)
+                        onNewLayout(layout.readLayout())
+                    } catch (e: Throwable) {
+                        System.err.println(e)
+                    }
                 }
             }
         }.start(wait = false)
--- ProcessingTest/src/main/java/com/thorebenoit/enamel/processingtest/kotlinapplet/applet/PlaygroundApplet.kt
+++ ProcessingTest/src/main/java/com/thorebenoit/enamel/processingtest/kotlinapplet/applet/PlaygroundApplet.kt
@@ -1,9 +1,12 @@
 package com.thorebenoit.enamel.processingtest.kotlinapplet.applet
 
 import com.thorebenoit.enamel.kotlin.core.math.i
+import com.thorebenoit.enamel.kotlin.core.print
 import com.thorebenoit.enamel.kotlin.geometry.figures.ESize
 import com.thorebenoit.enamel.kotlin.geometry.figures.ESizeType
 import com.thorebenoit.enamel.kotlin.geometry.figures.size
+import com.thorebenoit.enamel.kotlin.geometry.layout.ELayout
+import com.thorebenoit.enamel.kotlin.geometry.layout.playground.PlaygroundServer
 import processing.core.PConstants
 
 
@@ -29,4 +32,20 @@ class PlaygroundApplet : KotlinPAppletLambda() {
         }
     }
 
+}
+
+fun main() {
+
+    PlaygroundApplet.start(800, 800) {
+        var layout: ELayout? = null
+        PlaygroundServer().start {
+            layout = it
+            it.print
+        }
+        onDraw {
+            background(255)
+            layout?.arrange(eframe)
+            layout?.draw()
+        }
+    }
 }
