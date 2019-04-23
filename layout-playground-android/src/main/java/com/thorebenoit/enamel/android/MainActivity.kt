9fffae4e077ebdd47d8d916c7dbe02bf1e8fcba3
Benoit Thore <thorebenoit@gmail.com>
2019-04-23 23:06:08 +0100

    Restore main activity

--- layout-playground-android/src/main/java/com/thorebenoit/enamel/android/MainActivity.kt
+++ layout-playground-android/src/main/java/com/thorebenoit/enamel/android/MainActivity.kt
@@ -2,26 +2,13 @@ package com.thorebenoit.enamel.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.thorebenoit.enamel.android.demo.DemoUI
-import com.thorebenoit.enamel.android.examples.demoView1
-import com.thorebenoit.enamel.android.examples.helloWorld
-import com.thorebenoit.enamel.android.examples.helloWorldTransition
-import com.thorebenoit.enamel.layout_android.startServer
+import com.thorebenoit.enamel.android.examples.*
 
 class MainActivity : AppCompatActivity() {
 
-    val ui by lazy { DemoUI(this) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(helloWorld(this))
-        setContentView(ui.home)
-//        setContentView(demoView1())
+        setContentView(helloWorld(this))
     }
 
-    override fun onBackPressed() {
-        if (!ui.onBackPressed()) {
-            super.onBackPressed()
-        }
-    }
 }
