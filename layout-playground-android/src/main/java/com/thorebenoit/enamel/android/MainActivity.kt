64d2aa29b2e48a00fe45c1cc739c8f51d99e06ca
Benoit Thore <thorebenoit@gmail.com>
2019-04-23 23:05:32 +0100

    bug fixes

--- layout-playground-android/src/main/java/com/thorebenoit/enamel/android/MainActivity.kt
+++ layout-playground-android/src/main/java/com/thorebenoit/enamel/android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.thorebenoit.enamel.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.thorebenoit.enamel.android.demo.DemoUI
 import com.thorebenoit.enamel.android.examples.demoView1
 import com.thorebenoit.enamel.android.examples.helloWorld
 import com.thorebenoit.enamel.android.examples.helloWorldTransition
@@ -9,9 +10,18 @@ import com.thorebenoit.enamel.layout_android.startServer
 
 class MainActivity : AppCompatActivity() {
 
+    val ui by lazy { DemoUI(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(helloWorld(this))
+//        setContentView(helloWorld(this))
+        setContentView(ui.home)
+//        setContentView(demoView1())
     }
 
+    override fun onBackPressed() {
+        if (!ui.onBackPressed()) {
+            super.onBackPressed()
+        }
+    }
 }
