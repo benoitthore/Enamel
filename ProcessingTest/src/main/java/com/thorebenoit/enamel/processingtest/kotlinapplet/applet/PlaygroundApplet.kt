46b6cb9dc886ec4e78437414af13445af484bc92
Benoit Thore <thorebenoit@gmail.com>
2019-04-28 20:52:24 +0100

    Cleanup master

--- ProcessingTest/src/main/java/com/thorebenoit/enamel/processingtest/kotlinapplet/applet/PlaygroundApplet.kt
+++ ProcessingTest/src/main/java/com/thorebenoit/enamel/processingtest/kotlinapplet/applet/PlaygroundApplet.kt
@@ -1,18 +1,7 @@
 package com.thorebenoit.enamel.processingtest.kotlinapplet.applet
 
-import com.thorebenoit.enamel.core.of
-import com.thorebenoit.enamel.core.print
-import com.thorebenoit.enamel.geometry.alignement.EAlignment
 import com.thorebenoit.enamel.geometry.figures.ESize
 import com.thorebenoit.enamel.geometry.figures.size
-import com.thorebenoit.enamel.geometry.layout.ELayout
-import com.thorebenoit.enamel.geometry.layout.dsl.*
-import com.thorebenoit.enamel.geometry.layout.ELayoutLeaf
-import com.thorebenoit.enamel.geometry.layout.playground.PlaygroundServer
-import com.thorebenoit.enamel.geometry.layout.playground.sendToPlayground
-import com.thorebenoit.enamel.geometry.layout.serializer.ELayoutDeserializer
-import com.thorebenoit.enamel.kotlin.core.color.randomColor
-import com.thorebenoit.enamel.kotlin.core.color.*
 
 
 class PlaygroundApplet : KotlinPAppletLambda() {
@@ -38,51 +27,4 @@ class PlaygroundApplet : KotlinPAppletLambda() {
         }
     }
 
-}
-
-fun main() {
-
-    val deserializer = ELayoutDeserializer()
-
-    PlaygroundApplet.start(800, 800) {
-        var layout: ELayout? = null
-
-        layout = 3.of { ELayoutLeaf(color = red) }
-            .mapIndexed { i, layout ->
-                layout.sizedSquare((i + 1) * 100)
-            }
-            .stacked(EAlignment.rightCenter, spacing = 10)
-            .arranged(EAlignment.topLeft)
-            .padded(20)
-
-        PlaygroundServer.start(deserializer = deserializer) {
-            layout = it
-            it.print
-        }
-        onDraw {
-            background(255)
-            layout?.arrange(eframe)
-            layout?.draw()
-        }
-    }
-
-
-    val colors = (0..10).map { randomColor() }
-    50.of {
-        ELayoutLeaf(
-            color = red
-//            colors.random()
-        )
-    }
-        .mapIndexed { i, layout ->
-            //        layout.sizedSquare((i + 1) * 100)
-            layout.sizedSquare(50)
-        }
-        .justified(EAlignment.rightCenter)
-        .snugged()
-        .arranged(EAlignment.topLeft)
-        .padded(20)
-
-
-        .sendToPlayground()
 }
