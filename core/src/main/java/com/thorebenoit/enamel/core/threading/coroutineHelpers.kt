3e623de7f05b413ddf0432e14f0ed2b0b3ae1d0c
Benoit Thore <benoitthore@40030301-BenoitThore.local>
2019-05-02 15:02:24 +0100

    Adding multidexing

--- core/src/main/java/com/thorebenoit/enamel/core/threading/coroutineHelpers.kt
+++ core/src/main/java/com/thorebenoit/enamel/core/threading/coroutineHelpers.kt
@@ -7,8 +7,7 @@ import java.util.concurrent.Executors
 fun <T> Deferred<T>.blockingGet(): T = runBlocking { await() }
 
 
-fun singleThreadCoroutine(block: suspend CoroutineScope.() -> Unit) =
-    GlobalScope.launch(Executors.newSingleThreadExecutor().asCoroutineDispatcher(), block = block)
+
 
 fun coroutine(block: suspend CoroutineScope.() -> Unit) = GlobalScope.launch(block = block)
 
@@ -19,18 +18,32 @@ inline fun coroutineDelayed(delay: Long = 100, crossinline block: suspend Corout
     })
 
 
-suspend fun <T> Channel<T>.forEach(block: (T) -> Unit): List<T> {
+
+fun <T> List<T>.asChannel(): Channel<T> {
+    val retChannel = Channel<T>()
+
+    coroutine {
+        forEach { retChannel.send(it) }
+        retChannel.close()
+    }
+    return retChannel
+}
+
+suspend fun <T> Channel<T>.toList(): List<T> {
     val list = mutableListOf<T>()
 
     try {
-        while (true) {
-            block(receive())
+        val iterator = iterator()
+        while (iterator.hasNext()) {
+            list += iterator.next()
         }
     } catch (e: Throwable) {
     }
 
     return list
 }
+
+
 
 suspend fun <T> Channel<T>.receiveAll(): List<T> {
     val list = mutableListOf<T>()
@@ -44,19 +57,3 @@ suspend fun <T> Channel<T>.receiveAll(): List<T> {
 
     return list
 }
-
-suspend fun <T> Channel<T>.take(count: Int): List<T> {
-    val list = mutableListOf<T>()
-
-    try {
-        for (i in 0 until count) {
-            list.add(receive())
-        }
-    } catch (e: Throwable) {
-    }
-
-    return list
-}
-
-
-
