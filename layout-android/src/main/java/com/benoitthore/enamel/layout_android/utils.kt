c3fde3661eb6f1c527d2f802547cf6621aac5904
Benoit Thore <thorebenoit@gmail.com>
2019-08-19 12:19:13 +0100

    adding function in android's utils.kt

--- layout-android/src/main/java/com/benoitthore/enamel/layout_android/utils.kt
+++ layout-android/src/main/java/com/benoitthore/enamel/layout_android/utils.kt
@@ -1,6 +1,9 @@
 package com.benoitthore.enamel.layout_android
 
+import android.content.Context
+import android.content.res.Resources
 import android.view.View
+import com.benoitthore.enamel.geometry.layout.ELayout
 import com.benoitthore.enamel.geometry.layout.refs.ELayoutRef
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,3 +24,15 @@ fun <T : View> ELayoutRef<T>.withTag(tag: Any): ELayoutRef<T> = apply {
 
 
 val <T : View> ELayoutRef<T>.view get() = ref.viewRef
+
+
+fun Context.eViewGroup(block: EViewGroup.() -> ELayout): EViewGroup = EViewGroup(
+    this
+).apply {
+    layout = block()
+}
+
+val Number.dp: Int
+    get() = (toFloat() * Resources.getSystem().displayMetrics.density).toInt()
+
+
