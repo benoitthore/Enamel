16d33f822b7025ef6d2e2d53753d54b8eaf9f87b
Benoit Thore <thorebenoit@gmail.com>
2019-08-29 15:29:24 +0100

    equallySized function uses EAlignement instead of ELayoutAxis

--- layout/src/main/java/com/benoitthore/enamel/geometry/layout/dsl/sizing.kt
+++ layout/src/main/java/com/benoitthore/enamel/geometry/layout/dsl/sizing.kt
@@ -11,7 +11,8 @@ import com.benoitthore.enamel.geometry.layout.EWeightLayout
 import com.benoitthore.enamel.geometry.primitives.Tuple2
 
 
-fun List<ELayout>.justified(alignment: EAlignment) = EJustifiedLayout(this.toMutableList(), alignment)
+fun List<ELayout>.justified(alignment: EAlignment) =
+    EJustifiedLayout(this.toMutableList(), alignment)
 
 fun ELayout.sized(size: ESize) = ESizingLayout(this, ESizingLayout.ELayoutSpace.Size(size))
 fun ELayout.sized(width: Number, height: Number) = sized(width size height)
@@ -20,13 +21,19 @@ fun ELayout.sizedSquare(size: Number) = sized(size size size)
 fun ELayout.width(width: Number) = ESizingLayout(this, ESizingLayout.ELayoutSpace.Width(width))
 fun ELayout.height(height: Number) = ESizingLayout(this, ESizingLayout.ELayoutSpace.Height(height))
 
-fun ELayout.fitting(size: ESize) = ESizingLayout(this, ESizingLayout.ELayoutSpace.AspectFitting(size))
+fun ELayout.fitting(size: ESize) =
+    ESizingLayout(this, ESizingLayout.ELayoutSpace.AspectFitting(size))
+
 fun ELayout.fitting(width: Number, height: Number) = fitting(width size height)
 
-fun ELayout.filling(size: ESize) = ESizingLayout(this, ESizingLayout.ELayoutSpace.AspectFilling(size))
+fun ELayout.filling(size: ESize) =
+    ESizingLayout(this, ESizingLayout.ELayoutSpace.AspectFilling(size))
+
 fun ELayout.filling(width: Number, height: Number) = filling(width size height)
 
-fun ELayout.scaled(x: Number? = null, y: Number? = null) = ESizingLayout(this, ESizingLayout.ELayoutSpace.Scale(x, y))
+fun ELayout.scaled(x: Number? = null, y: Number? = null) =
+    ESizingLayout(this, ESizingLayout.ELayoutSpace.Scale(x, y))
+
 fun ELayout.scaled(tuple2: Tuple2) = scaled(tuple2.v1, tuple2.v2)
 
 // Lists
@@ -48,12 +55,9 @@ fun List<ELayout>.scaled(tuple2: Tuple2) = map { it.scaled(tuple2) }
 fun List<ELayout>.scaled(x: Number, y: Number) = map { it.scaled(x, y) }
 
 // Weights
-fun List<ELayout>.equallySized(alignment: ELayoutAxis, spacing: Number = 0) =
+fun List<ELayout>.equallySized(alignment: EAlignment, spacing: Number = 0) =
     EWeightLayout(
-        alignment = when(alignment){
-            ELayoutAxis.vertical -> EAlignment.bottomCenter
-            ELayoutAxis.horizontal -> EAlignment.rightCenter
-        },
+        alignment = alignment,
         childLayouts = this,
         weights = List(size) { 1 },
         spacing = spacing
