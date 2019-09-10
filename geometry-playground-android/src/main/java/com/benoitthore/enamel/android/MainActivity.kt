613f005d9f0a9f901d598dc0df4c53cb3d98f861
Benoit Thore <thorebenoit@gmail.com>
2019-09-10 14:05:27 +0100

    Update base example

--- geometry-playground-android/src/main/java/com/benoitthore/enamel/android/MainActivity.kt
+++ geometry-playground-android/src/main/java/com/benoitthore/enamel/android/MainActivity.kt
@@ -1,24 +1,87 @@
 package com.benoitthore.enamel.android
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.benoitthore.enamel.core.math.f
 import com.benoitthore.enamel.core.print
+import com.benoitthore.enamel.core.threading.coroutine
 import com.benoitthore.enamel.core.time.ETimer
 import com.benoitthore.enamel.geometry.AllocationTracker
 import com.benoitthore.enamel.geometry.alignement.EAlignment.center
+import com.benoitthore.enamel.geometry.alignement.EAlignment.rightCenter
 import com.benoitthore.enamel.geometry.figures.ECircle
 import com.benoitthore.enamel.geometry.figures.ECircleMutable
-import com.benoitthore.enamel.geometry.layout.dsl.arranged
+import com.benoitthore.enamel.geometry.innerCircle
+import com.benoitthore.enamel.geometry.layout.dsl.*
 import com.benoitthore.enamel.geometry.primitives.AngleType
 import com.benoitthore.enamel.geometry.primitives.degrees
 import com.benoitthore.enamel.geometry.primitives.radians
 import com.benoitthore.enamel.geometry.toCircle
 import com.benoitthore.enamel.layout.android.dp
 import com.benoitthore.enamel.layout.android.eViewGroup
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.contracts.ExperimentalContracts
+
+fun Context.sleepCanvasView(sleep: Long? = null): CanvasTestView {
+    val pool = GeometryPool(100)
+
+    val paint = Paint()
+
+    val timer = ETimer()
+    val targetFrameDuration = 16f
+    val frameIncrease = 1 / 60f
+
+    val angle = 0.degrees()
+
+    return canvasView { canvas ->
+
+        val deltaTime = if (timer.isStarted) {
+            timer.elapsed / targetFrameDuration
+        } else 1f
+        timer.start()
+
+        deltaTime.print
+        pool.apply {
+
+            canvas.drawColor(Color.RED)
+            paddedFrame { frame ->
+                frame.innerCircle(circlePool())
+                    .selfInset(width * 0.05)
+                    .toListOfPoint(
+                        pointPool.list(50),
+                        angle.selfOffset((deltaTime * frameIncrease).radians(anglePool()))
+                    )
+                    .map { it.toCircle(width * 0.01, circlePool()) }
+                    .forEachIndexed { i, it: ECircle ->
+                        paint.strokeWidth = 0.dp.f
+                        paint.color =
+                            if (i == 0) Color.WHITE else Color.BLACK
+                        it.draw(paint)
+
+                        linePool().let { line ->
+                            paint.color = Color.CYAN
+                            paint.strokeWidth = width * 0.005f
+                            line.set(it.center, frame.center(pointPool())).draw(paint)
+                        }
+                    }
+
+            }
+        }
+
+        GlobalScope.launch(Dispatchers.Main) {
+            sleep?.let {
+                delay(it)
+            }
+            invalidate()
+        }
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,59 +90,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         AllocationTracker.debugAllocations = true
 
-        val pool = GeometryPool(100)
 
-        val paint = Paint()
-
-        val timer = ETimer()
-        val targetFrameDuration = 16f
-        val frameIncrease = 1 / 60f
-
-        val angle = 0.degrees()
-
-        val canvasView = canvasView { canvas ->
-
-            val deltaTime = if (timer.isStarted) {
-                timer.elapsed / targetFrameDuration
-            } else 1f
-            timer.start()
-
-            deltaTime.print
-            pool.apply {
-
-                paddedFrame { frame ->
-                    frame.center(pointPool.next)
-                        .toCircle(frame.width / 2, circlePool.next)
-                        .selfInset(32.dp)
-                        .toListOfPoint(
-                            pointPool.list(50),
-                            angle.selfOffset((deltaTime * frameIncrease).radians(anglePool.next))
-                        )
-                        .map { it.toCircle(10.dp, circlePool.next) }
-                        .forEachIndexed { i, it: ECircle ->
-                            paint.strokeWidth = 0.dp.f
-                            paint.color = if (i == 0) Color.RED else Color.BLACK
-                            it.draw(paint)
-
-                            linePool().let { line ->
-                                paint.color = Color.CYAN
-                                paint.strokeWidth = 1.dp.f
-                                line.set(it.center, frame.center(pointPool())).draw(paint)
-                            }
-                        }
-
-                }
-            }
-
-            invalidate()
-        }
+        val sleepView = sleepCanvasView(1000)
+        val noSleepView = sleepCanvasView()
 
 
         setContentView(
             eViewGroup {
 
-
-                canvasView.laid().arranged(center)
+                listOf(sleepView, noSleepView).laid()
+                    .map { it.padded(4.dp) }
+                    .equallySized(rightCenter)
+                    .arranged(center)
             }
 
         )
