f9c3200067e7390adda50f74fe06601a5cea99c7
Benoit Thore <thorebenoit@gmail.com>
2019-09-13 01:13:28 +0100

    Geometry pool now throws Exceptions when overflowing

--- geometry-playground-android/src/main/java/com/benoitthore/enamel/android/extract/Pool.kt
+++ geometry-playground-android/src/main/java/com/benoitthore/enamel/android/extract/Pool.kt
@@ -30,12 +30,18 @@ import com.benoitthore.enamel.geometry.primitives.EPointMutable
     val anglePool = AnglePool()
  */
 
+
 //interface
 class Pool<T : Any>(val size: Int, val init: (Int) -> T) {
     private val list = List(size, init)
     private var i = 0
-        val next get() = list[i++ % size]
-//    val next get() = init(0)
+    val next: T
+        get() {
+            if (i + 1 > size) {
+                throw Exception("Pool overflow on ${list.firstOrNull()}")
+            }
+            return list[i++]
+        }
 
     operator fun invoke() = next
     operator fun invoke(size: Int) = list(size)
@@ -54,13 +60,19 @@ class Pool<T : Any>(val size: Int, val init: (Int) -> T) {
         return listBuffer
 
     }
+
+    fun reset() {
+        i = 0
+    }
 }
 
 fun RectPool(size: Int = 50) = Pool(size) { ERectMutable() }
 fun PointPool(size: Int = 50) =
     Pool(size) { EPointMutable() }
+
 fun CirclePool(size: Int = 50) =
     Pool(size) { ECircleMutable() }
+
 fun LinePool(size: Int = 50) = Pool(size) { ELineMutable() }
 fun AnglePool(size: Int = 50) =
     Pool(size) { EAngleMutable() }
@@ -84,6 +96,20 @@ class GeometryPool(
         LinePool(linePoolSize)
     val anglePool: Pool<EAngleMutable> =
         AnglePool(anglePoolSize)
+
+    inline fun use(block: GeometryPool.() -> Unit) {
+        reset()
+        block()
+        reset()
+    }
+
+    fun reset() {
+        rectPool.reset()
+        pointPool.reset()
+        circlePool.reset()
+        linePool.reset()
+        anglePool.reset()
+    }
 
 
 }
