2fee486bbdf5198a7bf3654b83971da522b576c7
Benoit Thore <thorebenoit@gmail.com>
2019-09-13 01:14:13 +0100

    Adding circular noise to fancyView

--- geometry-playground-android/src/main/java/com/benoitthore/enamel/android/MainActivity.kt
+++ geometry-playground-android/src/main/java/com/benoitthore/enamel/android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.benoitthore.enamel.android
 
+import android.animation.TimeInterpolator
 import android.content.Context
 import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import android.graphics.*
@@ -9,31 +10,21 @@ import androidx.appcompat.app.AppCompatActivity
 import com.benoitthore.enamel.android.demo.CanvasTestView
 import com.benoitthore.enamel.android.demo.canvasView
 import com.benoitthore.enamel.android.extract.*
-import com.benoitthore.enamel.core.math.Scale
+import com.benoitthore.enamel.core.animations.EasingInterpolators
 import com.benoitthore.enamel.core.math.*
 import com.benoitthore.enamel.core.time.ETimer
 import com.benoitthore.enamel.geometry.AllocationTracker
-import com.benoitthore.enamel.geometry.alignement.EAlignment.*
 import com.benoitthore.enamel.geometry.figures.ECircle
-import com.benoitthore.enamel.geometry.figures.ELine
 import com.benoitthore.enamel.geometry.figures.line
 import com.benoitthore.enamel.geometry.innerCircle
 import com.benoitthore.enamel.geometry.innerRect
-import com.benoitthore.enamel.geometry.layout.dsl.*
 import com.benoitthore.enamel.geometry.primitives.degrees
-import com.benoitthore.enamel.geometry.primitives.point
 import com.benoitthore.enamel.geometry.primitives.radians
-import com.benoitthore.enamel.geometry.primitives.rotations
 import com.benoitthore.enamel.geometry.toCircle
-import com.benoitthore.enamel.layout.android.dp
-import com.benoitthore.enamel.layout.android.eViewGroup
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import splitties.views.backgroundColor
 import kotlin.contracts.ExperimentalContracts
-import kotlin.math.min
 
 
 operator fun OpenSimplexNoise.invoke(x: Number) = eval(x.d)
@@ -45,25 +36,47 @@ val Context.isLandscape get() = resources.configuration.orientation == ORIENTATI
 fun Context.fancyView(): CanvasTestView {
     val pool = GeometryPool(100)
 
-    val paint = Paint()
-    paint.isAntiAlias = true
 
+    // Utils
+    val noise = OpenSimplexNoise()
     val timer = ETimer()
+    val paint = Paint()
+    val debugPaint = Paint().apply {
+        color = WHITE
+    }
+    paint.apply {
+        isAntiAlias = true
+    }
+
+    // Settings
     val targetFrameDuration = 16f
+
     val angleIncrease = 0.5 * 1 / 60f
-    val noiseOffsetIncrease = 1 / 60f
 
+    val noiseOffsetIncrease = 5f
+    val noiseZoom = 750f
+
+    val speedUpMultiplier = 1500
+
+    // Keep track of
     val angle = 0.degrees()
-
-
-    val noise = OpenSimplexNoise(System.currentTimeMillis())
-
-    var offset = 0f
+    var noiseOffset = 0f
 
 
     var shader: Shader? = null
-    return canvasView { canvas ->
-
+    return canvasView({
+        setOnClickListener {
+            var last = 0f
+            prepareAnimation {
+                noiseOffset += (it - last) * noiseOffsetIncrease * speedUpMultiplier
+                last = it
+            }.apply {
+                duration = 4000
+                interpolator = TimeInterpolator { input -> EasingInterpolators.cubicInOut(input) }
+                start()
+            }
+        }
+    }) { canvas ->
 
         canvas.drawColor(DKGRAY)
 
@@ -73,48 +86,64 @@ fun Context.fancyView(): CanvasTestView {
         timer.start()
 
 
-        offset += noiseOffsetIncrease * deltaTime
+        noiseOffset += noiseOffsetIncrease * deltaTime
 
-        pool.apply {
+        pool.use {
 
             paint.color = LTGRAY
             paint.style = Paint.Style.STROKE
+
+            // Define big circle
             frame.innerCircle(circlePool())
-                //.draw(paint)
                 .selfInset(width * 0.005)
-                .also { circle ->
+                // Set gradient shader
+                .also { circle: ECircle ->
                     if (shader == null)
                         circle.innerRect(rectPool())
-                            .selfScaleAnchor(0.75, 0.5, 0.5).let {
+                            .selfScaleAnchor(0.66, 0.5, 0.5).let {
                                 shader = (it.topLeft() line it.bottomRight())
                                     .toLinearGradient(
                                         (0 until 4).map { colorHSL(it / 4f) }
                                     )
                             }
                 }
-                .let { circle ->
+                // Small circles positions
+                .let { circle: ECircle ->
 
-                    circle.toListOfPoint(
+                    val points = circle.toListOfPoint(
                         pointPool.list(50),
-                        angle.selfOffset((deltaTime * angleIncrease).radians(anglePool())),
-                        distanceList = (0 until 50).map {
-                            circle.radius *
-                                    Scale.map((noise(offset + it / 10f)), -1, 1, 0.3, 1)
-                        }
+                        angle.selfOffset((deltaTime * angleIncrease).radians(anglePool()))
                     )
+
+
+                    // Point position with noise
+                    points.forEachIndexed { i, point ->
+                        val distance = noise(
+                            x = (noiseOffset + point.x) / noiseZoom,
+                            y = point.y / noiseZoom
+                        )
+                            .map(-1, 1, 0.33, 1) * circle.radius
+                        point.set(
+                            circle.center.offsetTowards(point, distance, pointPool())
+                        )
+                    }
+                    points
                 }
+                // Convert points to small circle
                 .map { it.toCircle(width * 0.01, circlePool()) }
-                .forEachIndexed { i, it: ECircle ->
+
+                // Draw
+                .forEachIndexed { i, circle: ECircle ->
                     paint.style = Paint.Style.FILL
                     paint.color = WHITE
 
                     paint.shader = shader
-                    it.draw(paint)
+                    circle.draw(paint)
 
                     linePool().let { line ->
                         paint.color = CYAN
                         paint.strokeWidth = width * 0.0025f
-                        line.set(it.center, frame.center(pointPool())).draw(paint)
+                        line.set(circle.center, frame.center(pointPool())).draw(paint)
                     }
                     paint.shader = null
                 }
