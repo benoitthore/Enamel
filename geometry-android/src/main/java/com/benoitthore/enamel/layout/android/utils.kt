f3852d35d0589e6b9c88561ceb28f9796fc5e2bf
Benoit Thore <thorebenoit@gmail.com>
2019-11-05 17:08:54 +0000

    remove internal utils

--- geometry-android/src/main/java/com/benoitthore/enamel/layout/android/utils.kt
+++
@@ -1,9 +0,0 @@
-package com.benoitthore.enamel.layout.android
-
-import android.content.res.Resources
-
-inline val Number.dp: Int
-    get() = (toFloat() * Resources.getSystem().displayMetrics.density).toInt()
-
-
-
