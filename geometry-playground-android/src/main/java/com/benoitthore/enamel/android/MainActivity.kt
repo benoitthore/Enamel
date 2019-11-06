c16d20ef7efb000cfcbb94589ad1c1d8b702ab0d
Benoit Thore <thorebenoit@gmail.com>
2019-11-06 00:38:44 +0000

    Adding Image Layout

--- geometry-playground-android/src/main/java/com/benoitthore/enamel/android/MainActivity.kt
+++ geometry-playground-android/src/main/java/com/benoitthore/enamel/android/MainActivity.kt
@@ -14,10 +14,7 @@ import android.text.TextPaint
 import androidx.appcompat.app.AppCompatActivity
 import com.benoitthore.enamel.android.demo.canvasView
 import com.benoitthore.enamel.geometry.AllocationTracker
-import com.benoitthore.enamel.geometry.figures.ERect
-import com.benoitthore.enamel.geometry.figures.ERectMutable
-import com.benoitthore.enamel.geometry.figures.ESize
-import com.benoitthore.enamel.geometry.figures.ESizeMutable
+import com.benoitthore.enamel.geometry.figures.*
 import com.benoitthore.enamel.geometry.layout.ELayout
 import com.benoitthore.enamel.geometry.layout.dsl.padded
 import com.benoitthore.enamel.geometry.layout.flowed
@@ -60,13 +57,13 @@ class MainActivity : AppCompatActivity() {
         GlobalScope.launch(Dispatchers.Main) {
             val image =
                 "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg"
-                    .downloadImage()
+                    .imageLayout()
 
             setContentView(canvasView { canvas ->
 
                 canvas.drawColor(DKGRAY)
 
-                canvas.drawBitmap(image, 0f, 0f, paint)
+                image.draw(canvas)
 
 //                wordLayout.arrange(frame)
 //                wordLayout.getAllChildren().forEach { layout ->
@@ -107,46 +104,70 @@ suspend inline fun String.downloadImage(crossinline block: RequestCreator.() -> 
     }
 
 
+suspend fun String.imageLayout(paint: Paint = Paint()) = EImageLayout(downloadImage(), paint)
+
 fun CharSequence.toWordLayout(paint: Paint) =
     split(" ")
         .filter { it.isNotBlank() }
         .map { EWordLayout(paint, it) }
         .flowed(lineSpacing = 8.dp, childSpacing = 16.dp)
 
+abstract class ECanvasLayout : ELayout {
+    override val children: List<ELayout> get() = emptyList()
+
+    private val _frame = ERectMutable()
+    val frame: ERect get() = _frame
+
+    override fun arrange(frame: ERect) {
+        _frame.set(frame)
+    }
+
+    abstract fun draw(canvas: Canvas)
+
+
+}
+
+class EImageLayout(
+    var image: Bitmap,
+    val paint: Paint
+) : ECanvasLayout() {
+
+    override fun size(toFit: ESize): ESize {
+        return image.width size image.height
+    }
+
+    override fun draw(canvas: Canvas) {
+        canvas.drawBitmap(image, frame.left, frame.top, paint)
+    }
+}
+
 class EWordLayout(
     val paint: Paint,
     text: CharSequence = ""
-) : ELayout {
+) : ECanvasLayout() {
 
     var text: CharSequence = text
         set(value) {
             field = value
             stringText = value.toString()
         }
+
     // Avoid allocation
     private var stringText = this.text.toString()
 
     override val children: List<ELayout> = emptyList()
-    private val _frame = ERectMutable()
-    val frame: ERect get() = _frame
+
 
     private val bufferSize = ESizeMutable()
 
-    override fun size(toFit: ESize): ESize {
-
-        return bufferSize.set(
-            paint.measureText(stringText),
-            paint.textSize + paint.descent()
-        )
-    }
+    override fun size(toFit: ESize): ESize = bufferSize.set(
+        paint.measureText(stringText),
+        paint.textSize + paint.descent()
+    )
 
 
-    override fun arrange(frame: ERect) {
-        _frame.set(frame)
-    }
-
-    fun draw(canvas: Canvas) {
-        canvas.drawText(stringText, _frame.x, _frame.y - paint.ascent(), paint)
+    override fun draw(canvas: Canvas) {
+        canvas.drawText(stringText, frame.x, frame.y - paint.ascent(), paint)
     }
 
 }
