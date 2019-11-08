e60d4a565ca192d6c19ddfbe4402368d76ccd7b6
Benoit Thore <thorebenoit@gmail.com>
2019-11-08 12:47:20 +0000

    Adding clicks

--- geometry-android/src/main/java/com/benoitthore/enamel/layout/android/extract/layout/ECanvasLayout.kt
+++ geometry-android/src/main/java/com/benoitthore/enamel/layout/android/extract/layout/ECanvasLayout.kt
@@ -1,12 +1,15 @@
 package com.benoitthore.enamel.layout.android.extract.layout
 
 import android.graphics.Canvas
+import android.view.View
 import com.benoitthore.enamel.geometry.figures.ERect
 import com.benoitthore.enamel.geometry.figures.ERectMutable
 import com.benoitthore.enamel.geometry.layout.ELayout
 import com.benoitthore.enamel.geometry.layout.refs.getAllChildren
 
 abstract class ECanvasLayout : ELayout {
+    // TODO Should there be a view reference on these layouts?
+//    abstract val view : View
     override val children: List<ELayout> get() = emptyList()
 
     private val _frame = ERectMutable()
@@ -19,8 +22,14 @@ abstract class ECanvasLayout : ELayout {
     abstract fun draw(canvas: Canvas)
 }
 
-
+// TODO Allocation
 fun ELayout.draw(canvas: Canvas) {
+    if (this is ECanvasLayout) {
+        draw(canvas)
+    }
+    if (children.isEmpty()) {
+        return
+    }
     getAllChildren().asSequence()
         .filterIsInstance<ECanvasLayout>()
         .forEach {
---
+++ geometry-android/src/main/java/com/benoitthore/enamel/layout/android/extract/layout/dsl.kt
@@ -0,0 +1,44 @@
+package com.benoitthore.enamel.layout.android.extract.layout
+
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.view.View
+import com.benoitthore.enamel.geometry.layout.ELayout
+import com.benoitthore.enamel.geometry.layout.flowed
+import com.benoitthore.enamel.geometry.layout.refs.getAllChildren
+import com.benoitthore.enamel.geometry.primitives.EPoint
+import com.benoitthore.enamel.layout.android.extract.SingleTouchDelegate
+import com.benoitthore.enamel.layout.android.extract.dp
+
+
+fun Drawable.imageLayout(paint: Paint = Paint()) = EImageLayout(drawable = this, paint = paint)
+
+// TODO Make 1 layout per line instead of 1 per word
+// TODO Handle \n
+// TODO Handle ellipsizing
+fun CharSequence.wordLayout(
+    paint: Paint,
+    lineSpacing: Number = 8.dp,
+    childSpacing: Number = 16.dp
+) =
+    split(" ").filter { it.isNotBlank() }.map { EWordLayout(paint, it) }
+        .flowed(lineSpacing = lineSpacing, childSpacing = childSpacing)
+
+fun ELayout.onClick(function: () -> Unit): EClickLayout = EClickLayout(this, function)
+
+fun ELayout.setupClicks(view: View) {
+    val touchLayouts = getAllChildren().filterIsInstance<EClickLayout>()
+
+    val touchListener =
+        SingleTouchDelegate { isDown: Boolean, current: EPoint?, previous: EPoint? ->
+            touchLayouts.forEach {
+                if (it.touchListener.invoke(isDown, current, previous)) {
+                    view.performClick()
+                    return@SingleTouchDelegate true
+                }
+            }
+            return@SingleTouchDelegate false
+        }
+
+    view.setOnTouchListener(touchListener)
+}
