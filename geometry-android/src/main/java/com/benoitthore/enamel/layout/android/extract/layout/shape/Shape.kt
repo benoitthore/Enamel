99d0aa076f72ac6b7773f363252f5c3b4aebc3c4
Benoit Thore <thorebenoit@gmail.com>
2019-11-27 01:26:36 +0000

    .

--- geometry-android/src/main/java/com/benoitthore/enamel/layout/android/extract/layout/shape/Shape.kt
+++ geometry-android/src/main/java/com/benoitthore/enamel/layout/android/extract/layout/shape/Shape.kt
@@ -1,19 +1,8 @@
 package com.benoitthore.enamel.layout.android.extract.layout.shape
 
-import android.graphics.Canvas
-import android.graphics.Paint
 import android.text.TextPaint
-import com.benoitthore.enamel.geometry.figures.ESize
 import com.benoitthore.enamel.layout.android.extract.layout.ECanvasLayout
 
 abstract class Shape : ECanvasLayout() {
-
-    protected val fillPaint: TextPaint = TextPaint().apply {
-        style = Paint.Style.FILL
-    }
-    protected val strokePaint: TextPaint = TextPaint().apply {
-        style = Paint.Style.STROKE
-    }
-
-
+    protected val paint: TextPaint = TextPaint()
 }
